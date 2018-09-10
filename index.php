cc3673606fa48b7ce4e73c49958765d66078ecaf
Künter Evert <kuntervert@users.noreply.github.com>
2018-09-10 10:41:13 +0300

    html muudetud phpks

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+h1 {
+    color: white;
+    text-shadow: 2px 2px 4px #000000;
+}
+</style>
+  <meta charset="utf-8">
+  <title> Künter Evert, veebiprogrammeerimine</title>
+</head>
+<body>
+  <h1>Evert</h1>
+  <p>Siin on minu õppetöö käigus valmistatud <a href="http://hugelol.com/" target="_blank" > veebileht</a>, millel puudub sisu ja tähtsus.</p>
+  <p>Loodan, et järgmised programmeerimistunnid on sama huvitavad ning omandan uusi ja kasulikke oskuseid.</p>
+	<!--<img src= "https://hugelolcdn.com/i/545627.jpg" alt="Kaunis meem">-->
+	<img src= "../545627.jpg" alt="Kaunis meem">
+	<p> Mu sõber Kert Liinat omab ka ühte väga lahedat <a href="../../~kertlii" target="_blank" >lehekülge </a>. </p>
+</body>
+</html>
